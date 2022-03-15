9b51132bfae67157b9a6af7a59d286731c88d854
persedawny <62754953+persedawny@users.noreply.github.com>
2022-03-15 19:25:01 +0100

    Update ICommand.ts

--- src/abstractions/ICommand.ts
+++ src/abstractions/ICommand.ts
@@ -1,7 +1,9 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { Message } from "discord.js";
+import { CustomClient } from "../client/customClient";
 
 export interface ICommand {
     data: SlashCommandBuilder;
+    client: CustomClient;
     execute: (interaction: Message) => void;
 }
