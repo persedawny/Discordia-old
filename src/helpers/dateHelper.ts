7f6fc01cf3616431b03d538fcdc4ecfdf5cb4306
persedawny <62754953+persedawny@users.noreply.github.com>
2022-03-21 21:45:20 +0100

    Update dateHelper.ts

--- src/helpers/dateHelper.ts
+++ src/helpers/dateHelper.ts
@@ -12,4 +12,36 @@ export class DateHelper {
     getCurrentDateMonthNumber(): number {
         return this.dateObj.getUTCMonth() + 1;
     }
+
+    getDateStringFromNumber(monthId: number): string{
+        switch (monthId) {
+            case 1:
+                return "January";
+            case 2:
+                return "February";
+            case 3:
+                return "March";
+            case 4:
+                return "April";
+            case 5:
+                return "May";
+            case 6:
+                return "June";
+            case 7:
+                return "July";
+            case 8:
+                return "August";
+            case 9:
+                return "September";
+            case 10:
+                return "October";
+            case 11:
+                return "November";
+            case 12:
+                return "December";
+        
+            default:
+                throw new Error("That is not a valid month!");
+        }
+    }
 }
