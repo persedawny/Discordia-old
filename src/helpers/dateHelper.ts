f0ec9ef7f93bed8af1432b430ad2fca9dafe4fe8
persedawny <62754953+persedawny@users.noreply.github.com>
2022-03-21 19:57:12 +0100

    Update dateHelper.ts

--- src/helpers/dateHelper.ts
+++ src/helpers/dateHelper.ts
@@ -1,4 +1,15 @@
 export class DateHelper {
+    dateObj: Date;
+
     constructor() {
+        this.dateObj = new Date();
+    }
+
+    getCurrentDateDayNumber(): number {
+        return this.dateObj.getUTCDate();
+    }
+
+    getCurrentDateMonthNumber(): number {
+        return this.dateObj.getUTCMonth() + 1;
     }
 }
